b3f9e2e2e9b7314763bd05621d4e7fdc40051961
Radu Crisan <radu.crisan@ekkode.com>
2024-04-26 11:22:49 +0300

    unse paranodid false and alter true

--- db-sync.js
+++ db-sync.js
@@ -2,7 +2,7 @@ import { sequelize } from "./db.js";
 
 import { Task  } from "./models/task.model.js";
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ alter: true }).then(() => {
   console.log("FINISHED SUCCESS");
   process.exit(0);
 });
