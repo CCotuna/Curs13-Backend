01a9c4a8a4a57e4dcd676f87c459a796d3687ec3
Radu Crisan <radu.crisan@ekkode.com>
2024-05-24 11:54:28 +0300

    ws add client

--- ws-server.js
+++ ws-server.js
@@ -2,13 +2,21 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+const clients = [];
+
 wss.on("connection", (ws) => {
   console.log("Client connected");
+
+  clients.push(ws);
 
   ws.on("message", function message(data) {
     console.log("received: %s", data);
 
-    ws.send(data.toString())
+    clients.forEach((wsClient)=> {
+        if (ws !== wsClient)
+            wsClient.send(data.toString())
+    })
+
   });
 
 //   const interval = setInterval(() => {
